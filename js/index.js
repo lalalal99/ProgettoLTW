9d69d8d5262cb9b8fbfc9edf11c1159f1568ccd2
lalalal99 <francesco.bucci99@hotmail.com>
2021-04-17 00:37:04 +0200

    locandine funzionanti

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
-var arr = ["Trees.png", "landscape.png"];
-length = 10;
+// var arr
+// var arr = ["Trees.png", "landscape.png"];
+// length = 10;
 
-function choose(choices) {
-  var index = Math.floor(Math.random() * choices.length);
-  return choices[index];
+// function choose(choices) {
+//   var index = Math.floor(Math.random() * choices.length);
+//   return choices[index];
+// }
+
+searchphrase = "hello";
+
+function startup() {
+  addScript(
+    "https://sg.media-imdb.com/suggests/" +
+      searchphrase[0] +
+      "/" +
+      searchphrase +
+      ".json"
+  );
 }
 
-function inEvidenza() {
+function addScript(src) {
+  var s = document.createElement("script");
+  s.src = src;
+  document.head.appendChild(s);
+}
+
+window["imdb$" + searchphrase] = Evidenza;
+
+function Evidenza(results) {
   var ul = document.getElementById("Evidenza_Lista");
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < results["d"].length; i++) {
     var li = document.createElement("li");
 
     var elem = document.createElement("img");
-    elem.setAttribute("src", "imgs/" + choose(arr));
+    elem.setAttribute("src", results["d"][i]["i"][0]);
+    // elem.setAttribute("src", "imgs/" + choose(arr));
     elem.setAttribute("class", "locandina");
 
     li.appendChild(elem);
