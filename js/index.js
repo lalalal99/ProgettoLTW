9aab75af3806154df359325bd3c3ef21290c07d2
lalalal99 <francesco.bucci99@hotmail.com>
2021-04-17 17:00:47 +0200

    Prima serata

--- js/index.js
+++ js/index.js
@@ -1,55 +1,115 @@
-// var arr
-// var arr = ["Trees.png", "landscape.png"];
-// length = 10;
-
-// function choose(choices) {
-//   var index = Math.floor(Math.random() * choices.length);
-//   return choices[index];
-// }
 OMDb_API = "9e44c172";
+items = [
+  "inception",
+  "iron_man",
+  "john_wick",
+  "wandavision",
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Godfather: Part II",
+  "Pulp Fiction",
+  "The Dark Knight",
+  "The Lord of the Rings: The Return of the King",
+  "Fight Club",
+];
+
 function startup() {
-  query = "hello";
-  richiesta(query);
+  generaEvidenza();
+  generaSerata();
 }
 
-function generaQuery(name) {
-  return "http://www.omdbapi.com/?apikey=" + OMDb_API + "&t=" + name;
+function generaEvidenza() {
+  evidenzaLenght = 10;
+  for (let i = 0; i < evidenzaLenght; i++) {
+    film = getFilm(getRandomTitle());
+    console.log(film);
+    aggiungiEvidenza(film);
+  }
 }
 
-function richiesta(src) {
-  var a = getFilm("inception");
-  console.log(a);
-
-  // jQuery
-  //   .ajax({
-  //     // url: generaQuery(src),
-  //     url: "http://www.omdbapi.com/?apikey=9e44c172&t=inception",
-  //     dataType: "json",
-  //     cache: true,
-  //   })
-  //   .then(Evidenza);
+function getRandomTitle() {
+  return items[Math.floor(Math.random() * items.length)];
 }
 
-function getFilm(src) {
+function getFilm() {
   //Ritorna un dizionario con le informazione del film src
   var value = $.ajax({
-    url: generaQuery(src),
+    url: generaQuery(arguments[0]),
     async: false,
   }).responseText;
   return JSON.parse(value);
 }
 
-function Evidenza(results) {
-  var ul = document.getElementById("Evidenza_Lista");
-  for (let i = 0; i < results["d"].length; i++) {
-    var li = document.createElement("li");
-
-    var elem = document.createElement("img");
-    elem.setAttribute("src", results["d"][i]["i"]);
-    elem.setAttribute("class", "locandina");
-
-    li.appendChild(elem);
-    ul.appendChild(li);
+function generaQuery() {
+  //genere link con titolo e eventuale true or false per trama lunga
+  var string =
+    "http://www.omdbapi.com/?apikey=" + OMDb_API + "&t=" + arguments[0];
+  if (arguments[1] == true) {
+    string += "&plot=full";
   }
-  console.log(results);
+
+  return string;
+}
+
+function aggiungiEvidenza(film) {
+  var ul = document.getElementById("Evidenza_Lista");
+  var li = document.createElement("li");
+  li.setAttribute("class", "elementoEvidenza");
+
+  var elem = document.createElement("img");
+  elem.setAttribute("src", film["Poster"]);
+  elem.setAttribute("class", "locandina");
+
+  li.appendChild(elem);
+  ul.appendChild(li);
+}
+
+function generaSerata() {
+  listaCanali = [
+    "Rai 1",
+    "Rai 2",
+    "Rai 3",
+    "Rete 4",
+    "Canale 5",
+    "Italia Uno",
+    "La Sette",
+    "tv8",
+    "nove",
+  ];
+  for (let i = 0; i < listaCanali.length; i++) {
+    aggiungiElementoSerata(listaCanali[i]);
+  }
+}
+
+function aggiungiElementoSerata(canale) {
+  var film = getFilm(getRandomTitle());
+  var ul = document.getElementById("Serata");
+  var li = document.createElement("li");
+
+  var div = document.createElement("div");
+  var tmp = document.createElement("h2");
+  tmp.appendChild(document.createTextNode(canale));
+  div.appendChild(tmp);
+
+  tmp = document.createElement("img");
+  tmp.setAttribute("src", film["Poster"]);
+  tmp.setAttribute("class", "locandina prima_serata");
+  div.appendChild(tmp);
+
+  var div1 = document.createElement("div");
+  tmp = document.createElement("h4");
+  tmp.appendChild(document.createTextNode(film["Title"]));
+  div1.appendChild(tmp);
+  tmp = document.createElement("h4");
+  tmp.appendChild(document.createTextNode(film["Runtime"]));
+  div1.appendChild(tmp);
+
+  tmp = document.createElement("p");
+  tmp.appendChild(document.createTextNode(film["Plot"]));
+  div1.appendChild(tmp);
+
+  div.appendChild(div1);
+
+  li.appendChild(div);
+  ul.appendChild(li);
 }
