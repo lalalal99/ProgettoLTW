36df99c3c165893f0a6762d466d52abb6faa40b4
lalalal99 <francesco.bucci99@hotmail.com>
2021-04-23 00:14:11 +0200

    impronta palinsesto

--- js/palinsesto_generator.js
+++ js/palinsesto_generator.js
@@ -1,18 +1,27 @@
 function generaPalinsesto() {
   $.getJSON("../data.txt", function (film) {
-    // console.log(film);
     test(film);
+    IDs = indiciRandom();
+    palinsesto = assemblaPalinsesto(IDs);
+    writeOnLocalStorage(film["tt0035423"]);
+    // writeOnLocalStorage(palinsesto);
   });
-  //["tt0035423"];
 }
 
-//scrivi il palinsesto su localstorage
-function test(film) {
-  //   console.log(film);
-  writeOnLocalStorage(film);
+//["tt0035423"];
+
+function indiciRandom() {
+  var IDs;
+  return IDs;
+}
+
+function assemblaPalinsesto(indici) {
+  var palinsesto;
+  return palinsesto;
 }
 
 function writeOnLocalStorage(dizionario) {
+  //scrivi il palinsesto su localstorage
   if (typeof localStorage.palinsesto == "undefined")
     localStorage.palinsesto = "";
   localStorage.palinsesto = JSON.stringify(dizionario);
